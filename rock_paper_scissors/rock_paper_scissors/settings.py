8274d1ef0b6afb38baad23da7ff03c7bcc23cce4
smkh1400 <mz2012kh82@gmail.com>
2023-11-11 22:27:19 +0330

    add postgresql

--- rock_paper_scissors/rock_paper_scissors/settings.py
+++ rock_paper_scissors/rock_paper_scissors/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'rock_paper_scissors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'backend_database',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
